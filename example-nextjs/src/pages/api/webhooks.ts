12b458b7f158a966b045d550691e0222fb3f1730
remorses <beats.by.morse@gmail.com>
2023-03-23 16:31:09 +0100

    example-nextjs/src/pages/api/webhooks:

--- example-nextjs/src/pages/api/webhooks.ts
+++ example-nextjs/src/pages/api/webhooks.ts
@@ -21,9 +21,30 @@ export default async function handler(
     await nodejsWebHookHandler({
         onData(payload) {
             console.log(payload)
-            if (payload.event_name === 'order_created') {
-                // payload.data is an Order
-                console.log(payload.data.attributes.status)
+
+            if (
+                payload.event_name === 'order_created' ||
+                payload.event_name === 'order_refunded'
+            ) {
+                // upsert order in database
+            } else if (
+                payload.event_name === 'subscription_created' ||
+                payload.event_name === 'subscription_cancelled' ||
+                payload.event_name === 'subscription_expired' ||
+                payload.event_name === 'subscription_paused' ||
+                payload.event_name === 'subscription_resumed' ||
+                payload.event_name === 'subscription_unpaused'
+            ) {
+                let sub = payload.data
+                // upsert subscription in database
+            } else if (payload.event_name === 'license_key_created') {
+                // upsert license key in database
+            } else if (
+                payload.event_name === 'subscription_payment_success' ||
+                payload.event_name === 'subscription_payment_failed' ||
+                payload.event_name === 'subscription_payment_recovered'
+            ) {
+                // do something when a subscription payment is successful, failed or recovered
             }
         },
         req,
