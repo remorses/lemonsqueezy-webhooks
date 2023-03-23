da5988f05e48edbee9a23df229d34f5560ec3d47
remorses <beats.by.morse@gmail.com>
2023-03-23 15:46:30 +0100

    init

---
+++ lemonsqueezy-webhooks/src/types.ts
@@ -0,0 +1,163 @@
+export type SubscriptionInvoice = {
+    type: 'subscription-invoices'
+    id: string
+    attributes: {
+        store_id: number
+        subscription_id: number
+        billing_reason: string
+        card_brand: string
+        card_last_four: string
+        currency: string
+        currency_rate: string
+        subtotal: number
+        discount_total: number
+        tax: number
+        total: number
+        subtotal_usd: number
+        discount_total_usd: number
+        tax_usd: number
+        total_usd: number
+        status: string
+        status_formatted: string
+        refunded: number
+        refunded_at: any
+        subtotal_formatted: string
+        discount_total_formatted: string
+        tax_formatted: string
+        total_formatted: string
+        urls: {
+            invoice_url: string
+        }
+        created_at: string
+        updated_at: string
+        test_mode: boolean
+    }
+    relationships: {
+        store: {
+            links: {
+                related: string
+                self: string
+            }
+        }
+        subscription: {
+            links: {
+                related: string
+                self: string
+            }
+        }
+    }
+    links: {
+        self: string
+    }
+}
+
+export type Subscription = {
+    type: 'subscriptions'
+    id: string
+    attributes: {
+        store_id: number
+        order_id: number
+        order_item_id: number
+        product_id: number
+        variant_id: number
+        product_name: string
+        variant_name: string
+        user_name: string
+        user_email: string
+        status: SubscriptionStatus
+        status_formatted: string
+        pause: any | null
+        cancelled: boolean
+        trial_ends_at: string | null
+        billing_anchor: number
+        urls: {
+            update_payment_method: string
+        }
+        renews_at: string
+        /**
+         * If the subscription has as status of cancelled or expired, this will be an ISO-8601 formatted date-time string indicating when the subscription expires (or expired). For all other status values, this will be null.
+         */
+        ends_at: string | null
+        created_at: string
+        updated_at: string
+        test_mode: boolean
+    }
+}
+
+export type Order = {
+    type: 'orders'
+    id: string
+    attributes: {
+        store_id: number
+        identifier: string
+        order_number: number
+        user_name: string
+        user_email: string
+        currency: string
+        currency_rate: string
+        subtotal: number
+        discount_total: number
+        tax: number
+        total: number
+        subtotal_usd: number
+        discount_total_usd: number
+        tax_usd: number
+        total_usd: number
+        tax_name: string
+        tax_rate: string
+        status: string
+        status_formatted: string
+        refunded: number
+        refunded_at: any
+        subtotal_formatted: string
+        discount_total_formatted: string
+        tax_formatted: string
+        total_formatted: string
+        first_order_item: {
+            id: number
+            order_id: number
+            product_id: number
+            variant_id: number
+            product_name: string
+            variant_name: string
+            price: number
+            created_at: string
+            updated_at: string
+            test_mode: boolean
+        }
+        created_at: string
+        updated_at: string
+    }
+}
+
+export type LicenseKey = {
+    type: 'license-keys'
+    id: string
+    attributes: {
+        store_id: number
+        order_id: number
+        order_item_id: number
+        product_id: number
+        user_name: string
+        user_email: string
+        key: string
+        key_short: string
+        activation_limit: number
+        instances_count: number
+        disabled: number
+        status: string
+        status_formatted: string
+        expires_at: any
+        created_at: string
+        updated_at: string
+    }
+}
+
+type SubscriptionStatus =
+    | 'on_trial'
+    | 'active'
+    | 'paused'
+    | 'past_due'
+    | 'unpaid'
+    | 'cancelled'
+    | 'expired'
